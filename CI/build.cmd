44baac9f997c5ef2808aaf7e48476d63bdc29ce1
Zachary Lund <zachary.lund@streamlabs.com>
2018-03-12 11:29:23 -0700

    Update appveyor script to build obs-browser

--- CI/build.cmd
+++ CI/build.cmd
@@ -1,6 +1,15 @@
 cmake ^
 	-G"%CmakeGenerator%" ^
+	-A x64 ^
 	-T "host=x64" ^
+	-H"%CefPath%" ^
+	-B"%CefBuildPath%" ^
+	-DCEF_RUNTIME_LIBRARY_FLAG="/MD"
+
+cmake --build "%CefBuildPath%" --config Release
+
+cmake ^
+	-G"%CmakeGenerator%" ^
 	-H"%APPVEYOR_BUILD_FOLDER%" ^
 	-B"%BuildPath32%" ^
 	-DCMAKE_INSTALL_PREFIX="%InstallPath%" ^
@@ -12,7 +21,6 @@ cmake ^
 
 cmake ^
 	-G"%CmakeGenerator%" ^
-	-T "host=x64" ^
 	-H"%APPVEYOR_BUILD_FOLDER%" ^
 	-B"%BuildPath64%" ^
 	-A x64 ^
@@ -21,8 +29,12 @@ cmake ^
 	-DCOPIED_DEPENDENCIES=false ^
 	-DCOPY_DEPENDENCIES=true ^
 	-DENABLE_SCRIPTING=false ^
+	-DCEF_ROOT_DIR=%CefPath% ^
+	-DCEF_WRAPPER_DIR="%CefBuildPath%\libcef_dll_wrapper\Release" ^
+	-DUSE_STATIC_CRT=false ^
+	-DBUILD_BROWSER=true ^
 	-DCOMPILE_D3D12_HOOK=true
-	
+
 cmake --build "%BuildPath32%\plugins\win-capture\get-graphics-offsets" --config "%BuildConfig%" --target install
 cmake --build "%BuildPath32%\plugins\win-capture\graphics-hook" --config "%BuildConfig%" --target install
 cmake --build "%BuildPath32%\plugins\win-capture\inject-helper" --config "%BuildConfig%" --target install
