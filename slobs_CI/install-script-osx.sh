3d7a1f7a2ec671de36273f295db0070606e496db
eddyStreamlabs <eddy.gharbi@streamlabs.com>
2020-01-10 16:05:48 -0800

    Build libcef and browser source

--- slobs_CI/install-script-osx.sh
+++ slobs_CI/install-script-osx.sh
@@ -21,3 +21,19 @@ tar -xf ./osx-deps-2018-08-09.tar.gz -C /tmp
 hr "Downloading VLC repo"
 wget --quiet --retry-connrefused --waitretry=1 https://downloads.videolan.org/vlc/3.0.4/vlc-3.0.4.tar.xz
 tar -xf vlc-3.0.4.tar.xz
+
+# CEF Stuff
+hr "Downloading CEF"
+wget --quiet --retry-connrefused --waitretry=1 https://obs-studio-deployment.s3-us-west-2.amazonaws.com/cef_binary_${CEF_MAC_BUILD_VERSION}_macosx64.tar.bz2
+tar -xf ./cef_binary_${CEF_MAC_BUILD_VERSION}_macosx64.tar.bz2
+cd ./cef_binary_${CEF_MAC_BUILD_VERSION}_macosx64
+# remove a broken test
+sed -i '.orig' '/add_subdirectory(tests\/ceftests)/d' ./CMakeLists.txt
+# target 10.11
+sed -i '.orig' s/\"10.9\"/\"10.11\"/ ./cmake/cef_variables.cmake
+mkdir build
+cd ./build
+cmake -DCMAKE_CXX_FLAGS="-std=c++11 -stdlib=libc++" -DCMAKE_EXE_LINKER_FLAGS="-std=c++11 -stdlib=libc++" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.11 ..
+make -j4
+mkdir libcef_dll
+cd ../../
