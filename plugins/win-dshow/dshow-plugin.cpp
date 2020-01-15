ed76b28d4b24cb24a0a4ab88a72c5c9c73dc8a94
Pablo Damasceno <pabloronan@gmail.com>
2020-01-15 14:15:50 -0800

    Add win-dshow cache folder creation

--- plugins/win-dshow/dshow-plugin.cpp
+++ plugins/win-dshow/dshow-plugin.cpp
@@ -1,4 +1,5 @@
 #include <obs-module.h>
+#include <util/platform.h>
 
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE("win-dshow", "en-US")
@@ -12,6 +13,12 @@ extern void RegisterDShowEncoders();
 
 bool obs_module_load(void)
 {
+    char *config_dir = obs_module_config_path(nullptr);
+    if (config_dir) {
+        os_mkdirs(config_dir);
+        bfree(config_dir);
+    }
+
 	RegisterDShowSource();
 	RegisterDShowEncoders();
 	return true;
