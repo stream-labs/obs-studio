12518ebc156df378dd1657151eefb6f0d653ceca
eddyStreamlabs <eddy.gharbi@streamlabs.com>
2020-01-20 13:29:21 -0800

    Fix cmake

--- slobs_CI/build-script-osx.sh
+++ slobs_CI/build-script-osx.sh
@@ -5,21 +5,14 @@ git fetch --tags
 mkdir build packed_build
 
 cd build
-cmake \
-#-DCMAKE_OSX_DEPLOYMENT_TARGET=10.11 \
--DDepsPath=/tmp/obsdeps \
+cmake -DDepsPath=/tmp/obsdeps \
 -DCMAKE_INSTALL_PREFIX=$PWD/../packed_build \
 -DVLCPath=$PWD/../../vlc-3.0.4 \
 -DENABLE_UI=false \
 -DDISABLE_UI=true \
 -DCOPIED_DEPENDENCIES=false \
 -DCOPY_DEPENDENCIES=true \
-#-DENABLE_SCRIPTING=false \
 -DBUILD_BROWSER=ON \
-#-DBROWSER_DEPLOY=ON \
-#-DBUILD_CAPTIONS=ON \
-#-DBROWSER_FRONTEND_API_SUPPORT=false \
-#-DBROWSER_PANEL_SUPPORT=false \
 -DUSE_UI_LOOP=true \
 -DCEF_ROOT_DIR=$PWD/../../cef_binary_${CEF_MAC_BUILD_VERSION}_macosx64 ..
 
