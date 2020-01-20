f70703e3f27d13818b7bae66fab4ef27f7b3f41d
eddyStreamlabs <eddy.gharbi@streamlabs.com>
2020-01-20 15:28:53 -0800

    Fix cef framework loading issue

--- slobs_CI/build-script-osx.sh
+++ slobs_CI/build-script-osx.sh
@@ -5,14 +5,20 @@ git fetch --tags
 mkdir build packed_build
 
 cd build
-cmake -DDepsPath=/tmp/obsdeps \
+cmake -DCMAKE_OSX_DEPLOYMENT_TARGET=10.11 \
+-DDepsPath=/tmp/obsdeps \
 -DCMAKE_INSTALL_PREFIX=$PWD/../packed_build \
 -DVLCPath=$PWD/../../vlc-3.0.4 \
 -DENABLE_UI=false \
 -DDISABLE_UI=true \
 -DCOPIED_DEPENDENCIES=false \
 -DCOPY_DEPENDENCIES=true \
+-DENABLE_SCRIPTING=false
 -DBUILD_BROWSER=ON \
+-DBROWSER_DEPLOY=ON \
+-DBUILD_CAPTIONS=ON \
+-DBROWSER_FRONTEND_API_SUPPORT=false \
+-DBROWSER_PANEL_SUPPORT=false \
 -DUSE_UI_LOOP=true \
 -DCEF_ROOT_DIR=$PWD/../../cef_binary_${CEF_MAC_BUILD_VERSION}_macosx64 ..
 
@@ -40,13 +46,4 @@ cp ../../cef_binary_${CEF_MAC_BUILD_VERSION}_macosx64/Release/Chromium\ Embedded
 cp ../../cef_binary_${CEF_MAC_BUILD_VERSION}_macosx64/Release/Chromium\ Embedded\ Framework.framework/Libraries/libswiftshader_libGLESv2.dylib \
 ./obs-plugins/libswiftshader_libGLESv2.dylib
 
-# Apply new Framework load path
-sudo install_name_tool -change \
-    @executable_path/../Frameworks/Chromium\ Embedded\ Framework.framework/Chromium\ Embedded\ Framework \
-    @executable_path/Frameworks/Chromium\ Embedded\ Framework.framework/Chromium\ Embedded\ Framework \
-    ./obs-plugins/obs-browser.so
-
-sudo install_name_tool -change \
-    @executable_path/../Frameworks/Chromium\ Embedded\ Framework.framework/Chromium\ Embedded\ Framework \
-    @executable_path/Frameworks/Chromium\ Embedded\ Framework.framework/Chromium\ Embedded\ Framework \
-    ./obs-plugins/obs-browser-page
+sudo chmod +x ./obs-plugins/obs-browser-page
