44731da1a24337645b3db9e4970c0277e0a1e707
Vladimir Sumarov <summeroff@gmail.com>
2020-01-23 16:00:04 -0800

    use new types of rules to detect games from a list

--- plugins/win-capture/window-helpers.h
+++ plugins/win-capture/window-helpers.h
@@ -3,9 +3,16 @@
 #include <util/dstr.h>
 
 enum window_priority {
-	WINDOW_PRIORITY_CLASS,
-	WINDOW_PRIORITY_TITLE,
-	WINDOW_PRIORITY_EXE,
+	WINDOW_PRIORITY_CLASS = 0,
+	WINDOW_PRIORITY_TITLE = 1,
+	WINDOW_PRIORITY_EXE = 2,
+
+	WINDOW_PRIORITY_EXE_ONLY = 20,
+	WINDOW_PRIORITY_EXE_TITLE = 21,
+	WINDOW_PRIORITY_EXE_CLASS = 22,
+	WINDOW_PRIORITY_NOT_EXE_TITLE = 31,
+	WINDOW_PRIORITY_NOT_EXE_CLASS = 32,
+	WINDOW_PRIORITY_NOT_CLASS = 33,
 };
 
 enum window_search_mode {
@@ -27,6 +34,8 @@ extern void get_window_class(struct dstr *class, HWND hwnd);
 extern bool is_uwp_window(HWND hwnd);
 extern HWND get_uwp_actual_window(HWND parent);
 
+extern void get_captured_window_line(HWND hwnd, struct dstr * window_line);
+
 typedef bool (*add_window_cb)(const char *title, const char *class,
 			      const char *exe);
 
@@ -34,7 +43,7 @@ extern void fill_window_list(obs_property_t *p, enum window_search_mode mode,
 			     add_window_cb callback);
 
 extern void build_window_strings(const char *str, char **class, char **title,
-				 char **exe, bool *sli_mode);
+				 char **exe, bool *sli_mode, int *priority);
 
 extern HWND find_window(enum window_search_mode mode,
 			enum window_priority priority, const char *class,
