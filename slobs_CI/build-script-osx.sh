520fef55e36f849235cef880de537d59313f081a
Eddy Gharbi <eddygharbiema@gmail.com>
2020-02-03 15:19:48 -0800

    Correctly set loading path for obs deps part 2 (plugins)

--- slobs_CI/build-script-osx.sh
+++ slobs_CI/build-script-osx.sh
@@ -85,3 +85,13 @@ sudo install_name_tool -change /tmp/obsdeps/bin/libavutil.56.dylib @executable_p
 
 sudo install_name_tool -change /tmp/obsdeps/bin/libswresample.3.dylib @executable_path/libswresample.3.dylib $PWD/../packed_build/bin/libswresample.3.dylib
 sudo install_name_tool -change /tmp/obsdeps/bin/libavutil.56.dylib @executable_path/libavutil.56.dylib $PWD/../packed_build/bin/libswresample.3.dylib
+
+sudo install_name_tool -change /tmp/obsdeps/bin/libx264.155.dylib @executable_path/../libx264.155.dylib $PWD/../packed_build/obs-plugins/obs-x264.so
+
+sudo install_name tool -change /tmp/obsdeps/bin/libavcodec.58.dylib @executable_path/../libavcodec.58.dylib $PWD/../packed_build/obs-plugins/obs-ffmpeg.so
+sudo install_name tool -change /tmp/obsdeps/bin/libavfilter.7.dylib @executable_path/../libavfilter.7.dylib $PWD/../packed_build/obs-plugins/obs-ffmpeg.so
+sudo install_name tool -change /tmp/obsdeps/bin/libavdevice.58.dylib @executable_path/../libavdevice.58.dylib $PWD/../packed_build/obs-plugins/obs-ffmpeg.so
+sudo install_name tool -change /tmp/obsdeps/bin/libavutil.56.dylib @executable_path/../libavutil.56.dylib $PWD/../packed_build/obs-plugins/obs-ffmpeg.so
+sudo install_name tool -change /tmp/obsdeps/bin/libswscale.5.dylib @executable_path/../libswscale.5.dylib $PWD/../packed_build/obs-plugins/obs-ffmpeg.so
+sudo install_name tool -change /tmp/obsdeps/bin/libavformat.58.dylib @executable_path/../libavformat.58.dylib $PWD/../packed_build/obs-plugins/obs-ffmpeg.so
+sudo install_name tool -change /tmp/obsdeps/bin/libswresample.3.dylib @executable_path/../libswresample.3.dylib $PWD/../packed_build/obs-plugins/obs-ffmpeg.so
