46019b57b1136f0a8d568deb0b7f65f302fc19b8
EddyGharbi <eddygharbiema@gmail.com>
2020-02-24 14:25:30 -0800

    Prevent dead lock

--- libobs/obs-display.c
+++ libobs/obs-display.c
@@ -263,15 +263,13 @@ void obs_display_size(obs_display_t *display, uint32_t *width, uint32_t *height)
 uint32_t obs_display_create_iosurface(obs_display_t *display,
 							uint32_t width, uint32_t height)
 {
-	pthread_mutex_lock(&display->draw_info_mutex);
-
 	obs_enter_graphics();
+	pthread_mutex_lock(&display->draw_info_mutex);
 	gs_load_swapchain(display->swap);
 
 	uint32_t surfaceID = gs_create_iosurface(width, height);
-	obs_leave_graphics();
 
 	pthread_mutex_unlock(&display->draw_info_mutex);
-
+	obs_leave_graphics();
 	return surfaceID;
 }
