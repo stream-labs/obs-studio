d00254cfbc8f0f37d35d1cf22af58da5aa97d4cc
Pablo Damasceno <pabloronan@gmail.com>
2020-05-07 08:04:28 -0700

    Remove unused define

--- plugins/obs-outputs/rtmp-stream.c
+++ plugins/obs-outputs/rtmp-stream.c
@@ -31,7 +31,6 @@
 
 /* dynamic bitrate coefficients */
 #define DBR_INC_TIMER (30ULL * SEC_TO_NSEC)
-//#define DBR_TRIGGER_USEC (200ULL * MSEC_TO_USEC)
 #define MIN_ESTIMATE_DURATION_MS 1000
 #define MAX_ESTIMATE_DURATION_MS 2000
 
