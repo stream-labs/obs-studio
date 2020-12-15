5ba84327fb7dbf176855581edc406ba19ca2097e
Florin Petrovici <florin0x01@gmail.com>
2020-12-15 21:25:23 +0200

    Cache result inside bigsur function

--- libobs/obs.c
+++ libobs/obs.c
@@ -1899,13 +1899,6 @@ float obs_get_master_volume(void)
 
 static obs_source_t *obs_load_source_type(obs_data_t *source_data)
 {
-	static int is_bigsur = -1;
-	if (is_bigsur == -1) {
-		if (is_BigSur_OS())
-			is_bigsur = 1;	
-		else
-			is_bigsur = 0;
-	}
 	obs_data_array_t *filters = obs_data_get_array(source_data, "filters");
 	obs_source_t *source;
 	const char *name = obs_data_get_string(source_data, "name");
@@ -2001,13 +1994,10 @@ static obs_source_t *obs_load_source_type(obs_data_t *source_data)
 			obs_source_set_audio_mixers(source, 0x3F);
 		}
 	}
-	if (is_bigsur) {
-		obs_source_set_monitoring_type(
-			source, OBS_MONITORING_TYPE_NONE);	
-	} else {
-		obs_source_set_monitoring_type(
-			source, (enum obs_monitoring_type)monitoring_type);	
-	}
+
+    obs_source_set_monitoring_type(
+            source, is_BigSur_OS() ? OBS_MONITORING_TYPE_NONE 
+                : (enum obs_monitoring_type)monitoring_type);
 
 	obs_data_release(source->private_settings);
 	source->private_settings =
--- libobs/util/check-os.cpp
+++ libobs/util/check-os.cpp
@@ -9,15 +9,22 @@ int is_BigSur_OS() {
     #ifndef __APPLE__
         return 0;
     #endif
-    fstream f;
-    f.open("/System/Library/CoreServices/SystemVersion.plist", ios::in);
-    if (f.fail()) {
-        return 0;
+    static int status_resolved = -1;
+	if (status_resolved != -1) {
+		return status_resolved;
+	}
+    fstream file;
+    file.open("/System/Library/CoreServices/SystemVersion.plist", ios::in);
+    if (file.fail()) {
+        status_resolved = 0;
+        return status_resolved;
     }
-    string file_str((istreambuf_iterator<char>(f)), istreambuf_iterator<char>());
+    string file_str((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
     if (file_str.find("<key>ProductUserVisibleVersion</key>\n\t<string>11") != string::npos) {
-        f.close();
-        return 1;
+        status_resolved = 1;
+    } else {
+        status_resolved = 0;
     }
-    return 0;
+    file.close();
+    return status_resolved;
 }
