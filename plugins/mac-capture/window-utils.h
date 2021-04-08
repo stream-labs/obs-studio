3e51e4400af485e3beacea59d3be8c1f7a34deba
Florin Petrovici <florin0x01@gmail.com>
2021-04-08 18:36:05 +0300

    Windowcapture permissions min. code with reverted PRs

--- plugins/mac-capture/window-utils.h
+++ plugins/mac-capture/window-utils.h
@@ -1,3 +1,5 @@
+#ifndef WINDOW_UTILS_H_
+#define WINDOW_UTILS_H_
 #import <CoreGraphics/CGWindow.h>
 #import <Cocoa/Cocoa.h>
 
@@ -14,6 +16,43 @@ struct cocoa_window {
 
 	uint64_t next_search_time;
 };
+
+enum crop_mode {
+	CROP_NONE,
+	CROP_MANUAL,
+	CROP_TO_WINDOW,
+	CROP_TO_WINDOW_AND_MANUAL,
+	CROP_INVALID
+};
+
+struct display_capture {
+	obs_source_t *source;
+
+	gs_samplerstate_t *sampler;
+	gs_effect_t *effect;
+	gs_texture_t *tex;
+	gs_vertbuffer_t *vertbuf;
+
+	NSScreen *screen;
+	unsigned display;
+	NSRect frame;
+	bool hide_cursor;
+
+	enum crop_mode crop;
+	CGRect crop_rect;
+
+	struct cocoa_window window;
+	CGRect window_rect;
+	bool on_screen;
+	bool hide_when_minimized;
+
+	os_event_t *disp_finished;
+	CGDisplayStreamRef disp;
+	IOSurfaceRef current, prev;
+
+	pthread_mutex_t mutex;
+};
+
 typedef struct cocoa_window *cocoa_window_t;
 
 NSArray *enumerate_cocoa_windows(void);
@@ -31,3 +70,5 @@ void window_defaults(obs_data_t *settings);
 void add_window_properties(obs_properties_t *props);
 
 void show_window_properties(obs_properties_t *props, bool show);
+
+#endif
