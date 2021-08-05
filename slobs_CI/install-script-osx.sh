cf72884ec85ac19d87e768ea2804d7596c732767
EddyGharbi <eddygharbiema@gmail.com>
2021-08-05 15:47:36 -0700

    vlc: fix more file extensions

--- slobs_CI/install-script-osx.sh
+++ slobs_CI/install-script-osx.sh
@@ -26,8 +26,8 @@ rm ./osx-deps-${MACOS_DEPS_VERSION}.tar.gz
 # Fetch vlc codebase
 hr "Downloading VLC repo"
 wget --quiet --retry-connrefused --waitretry=1 https://downloads.videolan.org/vlc/${VLC_VERSION}/vlc-${VLC_VERSION}.tar.xz
-tar -xf vlc-${VLC_VERSION}.tar.gz
-rm vlc-${VLC_VERSION}.tar.gz
+tar -xf vlc-${VLC_VERSION}.tar.xz
+rm vlc-${VLC_VERSION}.tar.xz
 
 # CEF Stuff
 hr "Downloading CEF"
