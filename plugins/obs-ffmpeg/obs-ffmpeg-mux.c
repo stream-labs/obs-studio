086bb3dda78bc4b71901438d512ea6eb0e5411e7
Eddy Gharbi <eddygharbiema@gmail.com>
2022-04-14 17:29:46 -0700

    obs-ffmpeg-mux: only send the wrote signal if the output was active

--- plugins/obs-ffmpeg/obs-ffmpeg-mux.c
+++ plugins/obs-ffmpeg/obs-ffmpeg-mux.c
@@ -400,6 +400,7 @@ int deactivate(struct ffmpeg_muxer *stream, int code)
 		     dstr_is_empty(&stream->printable_path)
 			     ? stream->path.array
 			     : stream->printable_path.array);
+		do_output_signal(stream->output, "wrote");
 	}
 
 	if (code) {
@@ -422,7 +423,6 @@ int deactivate(struct ffmpeg_muxer *stream, int code)
 	}
 
 	os_atomic_set_bool(&stream->stopping, false);
-	do_output_signal(stream->output, "wrote");
 	return ret;
 }
 
