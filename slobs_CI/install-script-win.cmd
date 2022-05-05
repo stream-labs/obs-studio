9c547c660b6c1e8179946a13ad9466affa4d4061
Vladimir Sumarov <summeroff@gmail.com>
2022-05-05 10:56:32 -0700

    fail build on cmake fail

--- slobs_CI/install-script-win.cmd
+++ slobs_CI/install-script-win.cmd
@@ -56,6 +56,7 @@ cmake -H. ^
 cmake --build %CD%\build --target install --config %BuildConfig% -v
 
 cmake --build %CD%\build --target check_dependencies --config %BuildConfig% -v
+if errorlevel 1 exit /B
 
 mkdir %CD%\%InstallPath%\data\obs-plugins\obs-virtualoutput
 move %CD%\build\%OBS_VIRTUALCAM% %CD%\%InstallPath%\data\obs-plugins\obs-virtualoutput\%OBS_VIRTUALCAM%
