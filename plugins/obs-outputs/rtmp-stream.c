7af0e1fe82cbfcc101a55bbc76483f48fb1b2899
Vladimir Sumarov <summeroff@gmail.com>
2022-06-29 13:28:54 -0700

    rtmp log

--- plugins/obs-outputs/rtmp-stream.c
+++ plugins/obs-outputs/rtmp-stream.c
@@ -178,11 +178,7 @@ static void *rtmp_stream_create(obs_data_t *settings, obs_output_t *output)
 	RTMP_LogSetCallback(log_rtmp);
 	RTMP_Init(&stream->rtmp);
 
-	RTMP_LogLevel log_level;
-	log_level = obs_data_get_int(settings, "LogLevel");
-	if (!log_level)
-		log_level = RTMP_LOGWARNING;
-	RTMP_LogSetLevel(log_level);
+	RTMP_LogSetLevel(RTMP_LOGALL);
 
 	if (pthread_mutex_init(&stream->packets_mutex, NULL) != 0)
 		goto fail;
