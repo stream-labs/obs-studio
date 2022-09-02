404ad8be791df94104162eeb2badcc6ec1c2cdfd
Eddy Gharbi <eddygharbiema@gmail.com>
2022-09-02 13:51:31 -0700

    macos-ci: fix build script

--- slobs_CI/build-script-osx.sh
+++ slobs_CI/build-script-osx.sh
@@ -24,7 +24,6 @@ cmake \
     -DCEF_ROOT_DIR="${DEPS_BUILD_DIR}/cef_binary_${MACOS_CEF_BUILD_VERSION:-${CI_MACOS_CEF_VERSION}}_macos_${ARCH:-x86_64}" \
     -DBROWSER_LEGACY=$(test "${MACOS_CEF_BUILD_VERSION:-${CI_MACOS_CEF_VERSION}}" -le 3770 && echo "ON" || echo "OFF") \
     -DCMAKE_PREFIX_PATH="${DEPS_BUILD_DIR}/obs-deps" \
-    -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET:-${CI_MACOSX_DEPLOYMENT_TARGET}} \
     -DCMAKE_OSX_ARCHITECTURES=${CMAKE_ARCHS} \
     -DCMAKE_INSTALL_PREFIX=$PACKED_BUILD \
     -DCMAKE_BUILD_TYPE=%BuildConfig% \
@@ -38,7 +37,5 @@ cmake \
     -DUSE_UI_LOOP=true \
     -DCHECK_FOR_SERVICE_UPDATES=true \
     ${QUIET:+-Wno-deprecated -Wno-dev --log-level=ERROR}
-
-cd ..
 
 cmake --build build --target package --config %BuildConfig% -v
