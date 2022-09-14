c512a4e4c89e6741e9aaf6c5b2b3f6be13dd03ff
Eddy Gharbi <eddygharbiema@gmail.com>
2022-09-14 12:34:51 -0700

    re enable building obs-browser

--- slobs_CI/win-build.cmd
+++ slobs_CI/win-build.cmd
@@ -34,7 +34,7 @@ cmake -H. ^
          -DBUILD_CAPTIONS=false ^
          -DCOMPILE_D3D12_HOOK=true ^
          -DBUILD_BROWSER=true ^
-         -DENABLE_BROWSER=false ^
+         -DENABLE_BROWSER=true ^
          -DENABLE_BROWSER_PANELS=false ^
          -DENABLE_BROWSER_QT_LOOP=false ^
          -DBROWSER_FRONTEND_API_SUPPORT=false ^
