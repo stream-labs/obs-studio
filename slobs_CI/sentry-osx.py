39c108804fd6a2b46356e0a3dbbb414a1081492f
unknown <stevensoftware52@gmail.com>
2023-01-15 14:03:06 -0500

    Test

--- slobs_CI/sentry-osx.py
+++ slobs_CI/sentry-osx.py
@@ -1,3 +1,25 @@
 import os
-print("1. Test")
 os.system('curl -sL https://sentry.io/get-cli/ | bash')
+
+def process_sentry(directory):
+    print("1. process_sentry directory is...")
+    print(directory)
+    for r, d, f in os.walk(directory):
+        for file in f:
+            print("1. file is...")
+            print(file)
+            if 'lib' in file or 'obs' in file or '.so' in file or '.dylib' in file:
+                path = os.path.join(directory, file)
+                os.system("dsymutil " + path)
+                os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-desktop --project obs-server " + path + ".dSYM/Contents/Resources/DWARF/" + file)
+    for r, d, f in os.walk(directory):
+        for file in f:
+            if 'lib' in file or 'obs' in file or '.so' in file or '.dylib' in file:
+                path = os.path.join(directory, file)
+                os.system("dsymutil " + path)
+                os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-desktop --project obs-server-preview " + path + ".dSYM/Contents/Resources/DWARF/" + file)
+
+# Upload obs debug files
+process_sentry(os.path.join(os.environ['PWD'], 'build', 'rundir', os.environ['BUILDCONFIG'], 'bin'))
+# Upload obs-plugins debug files
+process_sentry(os.path.join(os.environ['PWD'], 'build', 'rundir', os.environ['BUILDCONFIG'], 'obs-plugins'))
