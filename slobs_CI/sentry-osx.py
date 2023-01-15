55917ea6a7e8c7f7bd2ae1922af95c024ad86612
unknown <stevensoftware52@gmail.com>
2023-01-15 15:56:14 -0500

    Test

--- slobs_CI/sentry-osx.py
+++ slobs_CI/sentry-osx.py
@@ -4,7 +4,7 @@ os.system('curl -sL https://sentry.io/get-cli/ | bash')
 def process_sentry(directory):
     print("1. process_sentry directory is...")
     print(directory)
-    for root, dirs, files in os.walk(rootFolderPath):
+    for root, dirs, files in os.walk(directory):
         for file in files:
             print("2. filepath/root is...")
             print(os.path.join(root, file))
