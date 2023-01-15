70654a9aa3b6fcd108e3ac86fdec884ef4ff7b97
unknown <stevensoftware52@gmail.com>
2023-01-15 16:45:40 -0500

    Test

--- slobs_CI/sentry-osx.py
+++ slobs_CI/sentry-osx.py
@@ -6,11 +6,11 @@ def process_sentry(directory):
         for file in files:
             if 'lib' in file or 'obs' in file or '.so' in file or '.dylib' in file:
                 path = os.path.join(root, file)
-                print("Running sentry-cli on '" + path + "' to obs-server and obs-server-preview")
+                print("Running sentry-cli on '" + file + "' to obs-server and obs-server-preview, value = " + root + ".dSYM/Contents/Resources/DWARF/" + file)
                 os.system("dsymutil " + path)
-                os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-desktop --project obs-server " + path + ".dSYM/Contents/Resources/DWARF/" + file)                
+                os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-desktop --project obs-server " + root + ".dSYM/Contents/Resources/DWARF/" + file)                
                 os.system("dsymutil " + path)
-                os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-desktop --project obs-server-preview " + path + ".dSYM/Contents/Resources/DWARF/" + file)
+                os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-desktop --project obs-server-preview " + root + ".dSYM/Contents/Resources/DWARF/" + file)
 
 # Upload obs debug files
 process_sentry(os.path.join(os.environ['PWD'], 'build'))
