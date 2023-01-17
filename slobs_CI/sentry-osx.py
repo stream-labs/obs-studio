f594082a963f09a9a30e9a3cfa36677fb4d45a66
unknown <stevensoftware52@gmail.com>
2023-01-17 15:31:43 -0500

    Check files without extensions for debug info

--- slobs_CI/sentry-osx.py
+++ slobs_CI/sentry-osx.py
@@ -4,7 +4,7 @@ os.system('curl -sL https://sentry.io/get-cli/ | bash')
 def process_sentry(directory):
     for root, dirs, files in os.walk(directory):
         for file in files:
-            if 'lib' in file or 'obs' in file or '.so' in file or '.dylib' in file:
+            if 'lib' in file or 'obs' in file or '.so' in file or '.dylib' in file or '.' not in file:
                 path = os.path.join(root, file)
                 os.system("dsymutil " + path)
                 os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-desktop --project obs-server " + path + ".dSYM/Contents/Resources/DWARF/" + file)                
